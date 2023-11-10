be0cbb977c5f9bbefcbebf37070af9d488136aab
unhaten <csgold2002@mail.ru>
2023-11-11 02:34:16 +0300

    config edits

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        open: true,
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@use "@/assets/styles/variables.scss" as *;',
+            },
+        },
+    },
+});
