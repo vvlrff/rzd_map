b8de23cff4ed99286443dd8f7fe4cf5369eaaa1c
LegionaryByte <148693888+LegionaryByte@users.noreply.github.com>
2023-11-11 14:06:52 +0300

    [add_data_train_data]

--- backend/src/admin/router.py
+++ backend/src/admin/router.py
@@ -76,7 +76,21 @@ async def all_peregons(request_data: TrainIndexRequest, session: AsyncSession = 
     data = await support.all_peregons(train_index=request_data.train_index)
     return JSONResponse(data)
 
+@router.get('/add_data_train_data')
+async def add_data_train_data(session: AsyncSession = Depends(get_async_session)):
+    with open('TrainData.pickle', 'rb') as f:
+        loaded_obj = pickle.load(f)
 
+    for i in loaded_obj:
+        stmt = (
+            insert(TrainData)
+            .values(
+                train_index = i[1],
+                station_data = i[2]
+            )
+        )
+        await session.execute(stmt)
+        await session.commit()
 
 @router.post('/one_train_with_time')
 async def one_train_with_time(request_data: TrainIndexRequestCurrentData, session: AsyncSession = Depends(get_async_session)):
